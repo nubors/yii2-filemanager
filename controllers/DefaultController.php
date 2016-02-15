a1c589a11114aa7b1f297a5fc5dc7f59e4599b3f
Sena Yoga Samudra <sena@Senas-MacBook.local>
2016-02-15 14:54:51 +0700

    first commit

---
+++ controllers/DefaultController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace pendalf89\filemanager\controllers;
+
+use Yii;
+use yii\web\Controller;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+
+class DefaultController extends Controller
+{
+	public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'actions' => ['error'],
+                        'allow' => true,
+                    ],
+                    [
+                        'actions' => ['index', 'settings'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    public function actionSettings()
+    {
+        return $this->render('settings');
+    }
+}
